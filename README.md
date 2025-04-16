40e0fb4de8491e4345396fcea88aea8089306db7
Michel Gerding <michelgerding@gmail.com>
2025-04-16 10:24:02 +0200

    Initial commit. It should work for now

---
+++ README.md
@@ -0,0 +1,97 @@
+# odrive_interface
+This package is used to control an ODrive using ROS2. This package is developed for the Pieperkieker project.
+
+## File tree
+```
+odrive_interface
+├── CMakeLists.txt
+├── package.xml
+├── README.md
+├── arduino
+│   └── sketch
+│       └── sketch.ino
+├── msg
+│   ├── OdriveCommand.msg
+│   └── OdriveFeedback.msg
+└── src
+    └── odrive_serial_node.cpp
+```
+
+## ROS2 Interface for ODrive
+This package provides a simple interface to control an ODrive using ROS2. The interface is designed to be used with an Arduino that is connected to the ODrive via USB. The Arduino acts as a bridge between the ODrive and ROS2, allowing for easy control of the ODrive from a ROS2 node.
+
+## Dependencies
+The project has a couple of dependencies. this is split into two parts:
+- **Arduino**: 
+  - ODriveArduino: https://github.com/odriverobotics/ODriveArduino
+  - arduino-CAN: https://github.com/sandeepmistry/arduino-CAN
+- **ROS2**:
+  - ROS2 humble: https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debians.html
+  - PkgConfig: (sudo apt install pkg-config) https://www.freedesktop.org/wiki/Software/pkg-config/
+  - libserialport: (sudo apt install libserialport-dev) https://github.com/sigrokproject/libserialport
+ 
+## Protocol
+
+## Into odrive_interface
+The ros2 node has a couple topics it subscribes/pubishes to. these have custom message types. in this chapter these 
+topics and message types are described.
+
+### Message types
+The custom message types are defined in the `/msg` directory. The message types are as follows:
+- **OdriveCommand**: This message is used to send commands to the odrive. The message type is defined as follows:
+```
+string command
+int32 node_id
+float32 value
+```
+Where:
+- `command`: The command to be sent to the odrive. This can be one of the following:
+  - `P`: Set the position of the odrive in rotations
+  - `V`: Send the velocity of the odrive in rps
+- `node_id`: The id of the node to be controlled. This is an i32 value.
+- `value`: The value to be sent to the node. This is a f32 value.
+
+and finally the `OdriveFeedback` message type is used to send feedback from the odrive. The message type is defined as follows:
+```
+float32 position
+float32 velocity
+```
+Where:
+- `position`: The position of the odrive in rotations. This is a f32 value.
+- `velocity`: The velocity of the odrive in rps. This is a f32 value.
+
+### Topics
+
+The ros2 node listens and publishes on the following topics. on these topics custom messages types are used. these can be found in the `/msg` directory.
+- **Each node**:
+  - /odrive_{node_id}/command: This topic is used to send commands to the odrive. The message type is `OdriveCommand`.
+  - /odrive_{node_id}/feedback: This topic is used to receive feedback from the odrive. The message type is `OdriveFeedback`.
+- **Combined nodes**
+  - /throttle: This topic is used to send a message to all throttle motors this is of type `OdriveCommand`.
+  - /steering: This topic is used to send a message to all steering motors this is of type `OdriveCommand`.
+
+
+## Ros -> Arduino
+Ros and arduino communicate with each other via a serial port. This is doe using a simple plaintext protocol.
+The protocol is as follows:
+```
+{P|V} {node_id: int} {value: f32}
+```
+
+Where:
+- Command type: The type of command to be sent. This can be one of the following:
+    - `P`: Set the position of the odrive in rotations
+    - `V`: Send the velocity of the odrive in rps
+- Node id: The id of the node to be controlled. This is an i32 send as a string.
+- Value: The value to be sent to the node. This is a f32 value send as a string.
+
+### Feedback
+The odrive will also send feedback to the arduino. This is done using the following protocol:
+```
+OD{node_id: int}: Pos={position: f32} Vel={velocity: f32}
+```
+
+Where:
+- `node_id`: The id of the node to be controlled. This is an i32 send as a string.
+- `position`: The position of the odrive in rotations. This is a f32 value send as a string.
+- `velocity`: The velocity of the odrive in rps. This is a f32 value send as a string.
---
+++ arduino/sketch/sketch.ino
@@ -0,0 +1,167 @@
+#include <Arduino.h>
+#include "ODriveCAN.h"
+#include "MCP2515.h"
+#include "ODriveMCPCAN.hpp"
+
+#define POSITION_CONTROL_MODE 3
+#define VELOCITY_CONTROL_MODE 2
+
+
+#define CAN_BAUDRATE 250000
+#define MCP2515_CS 10
+#define MCP2515_INT 2
+#define MCP2515_CLK_HZ 8000000
+
+#define NUM_ODRIVES 1  // Change to match your number of ODrives
+
+MCP2515Class& can_intf = CAN;
+ODriveCAN* odrives[NUM_ODRIVES];
+
+struct ODriveUserData {
+  Heartbeat_msg_t last_heartbeat;
+  bool received_heartbeat = false;
+  Get_Encoder_Estimates_msg_t last_feedback;
+  bool received_feedback = false;
+};
+
+ODriveUserData odrive_user_data[NUM_ODRIVES];
+
+void onHeartbeat(Heartbeat_msg_t& msg, void* user_data) {
+  ((ODriveUserData*)user_data)->last_heartbeat = msg;
+  ((ODriveUserData*)user_data)->received_heartbeat = true;
+}
+
+void onFeedback(Get_Encoder_Estimates_msg_t& msg, void* user_data) {
+  ((ODriveUserData*)user_data)->last_feedback = msg;
+  ((ODriveUserData*)user_data)->received_feedback = true;
+}
+
+void onCanMessage(const CanMsg& msg) {
+  for (auto odrive : odrives) {
+    onReceive(msg, *odrive);
+  }
+}
+
+bool setupCan() {
+  CAN.setPins(MCP2515_CS, MCP2515_INT);
+  CAN.setClockFrequency(MCP2515_CLK_HZ);
+  if (!CAN.begin(CAN_BAUDRATE)) {
+    return false;
+  }
+  CAN.onReceive([](int packet_size) {
+    if (packet_size > 8) return;
+    CanMsg msg = {.id = (unsigned int)CAN.packetId(), .len = (uint8_t)packet_size};
+    CAN.readBytes(msg.buffer, packet_size);
+    onCanMessage(msg);
+  });
+  return true;
+}
+
+void setup() {
+  Serial.begin(115200);
+  for (int i = 0; i < 30 && !Serial; i++) delay(100);
+  Serial.println("Starting multi-ODrive controller");
+
+  if (!setupCan()) {
+    Serial.println("CAN failed to initialize.");
+    while (true);
+  }
+
+  for (int i = 0; i < NUM_ODRIVES; i++) {
+    odrives[i] = new ODriveCAN(wrap_can_intf(can_intf), i); // node_id = i
+    odrives[i]->onFeedback(onFeedback, &odrive_user_data[i]);
+    odrives[i]->onStatus(onHeartbeat, &odrive_user_data[i]);
+  }
+
+  Serial.println("Waiting for all ODrives...");
+  while (true) {
+    pumpEvents(can_intf);
+    bool all_ready = true;
+    for (int i = 0; i < NUM_ODRIVES; i++) {
+      if (!odrive_user_data[i].received_heartbeat) {
+        all_ready = false;
+        break;
+      }
+    }
+    if (all_ready) break;
+    delay(100);
+  }
+
+  Serial.println("All ODrives found. Enabling closed-loop control...");
+
+  for (int i = 0; i < NUM_ODRIVES; i++) {
+    odrives[i]->clearErrors();
+    odrives[i]->setState(ODriveAxisState::AXIS_STATE_CLOSED_LOOP_CONTROL);
+    delay(200);
+  }
+
+  Serial.println("Ready for Serial Commands: P|O|V <index> <value>");
+}
+
+void processSerialCommand(String line) {
+  char cmd;
+  int id;
+  char valStr[16];
+  float val;
+
+  line.trim();
+
+  if (sscanf(line.c_str(), "%c %d %s", &cmd, &id, valStr) == 3) {
+    val = atof(valStr);  // Convert the string to a float
+  } else {
+    Serial.print("Invalid command format: ");
+    Serial.print(line);
+    Serial.print(" - cmd: ");
+    Serial.print(cmd);
+    Serial.print(" id: ");
+    Serial.print(id);
+    Serial.print(" val: ");
+    Serial.print(val);
+    return;
+  }
+  if (id < 0 || id >= NUM_ODRIVES) {
+    Serial.println("Invalid ODrive index.");
+    return;
+  }
+
+  switch (cmd) {
+    case 'P':
+      odrives[id]->setControllerMode(POSITION_CONTROL_MODE, 3);
+      odrives[id]->setPosition(val);
+      break;
+    case 'V':
+      odrives[id]->setControllerMode(VELOCITY_CONTROL_MODE, 2);
+      odrives[id]->setVelocity(val);
+      break;
+    default:
+      Serial.println("Unknown command.");
+  }
+}
+
+void loop() {
+  pumpEvents(can_intf);
+
+  if (Serial.available()) {
+    String cmd = Serial.readStringUntil('\n');
+    cmd.trim();
+    if (cmd.length() > 0) {
+      processSerialCommand(cmd);
+    }
+  }
+
+  for (int i = 0; i < NUM_ODRIVES; i++) {
+    if (odrive_user_data[i].received_feedback) {
+      odrive_user_data[i].received_feedback = false;
+
+      float pos = odrive_user_data[i].last_feedback.Pos_Estimate;
+      float vel = odrive_user_data[i].last_feedback.Vel_Estimate;
+
+      Serial.print("OD");
+      Serial.print(i);
+      Serial.print(": Pos=");
+      Serial.print(pos, 3);
+      Serial.print(" Vel=");
+      Serial.println(vel, 3);
+    }
+  }
+}
